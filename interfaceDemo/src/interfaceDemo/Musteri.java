9f6c37c538d9ea878b162e2540b3006ba5661d3a
ElifEryigit <eliferyigit55@gmail.com>
2021-06-16 09:51:26 +0300

    deneme

---
+++ interfaceDemo/src/interfaceDemo/Musteri.java
@@ -0,0 +1,17 @@
+package interfaceDemo;
+
+public class Musteri implements Ikullanici,Iodeme{
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ interfaceDemo/src/interfaceDemo/Satici.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+
+public class Satici implements Ikullanici{
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
